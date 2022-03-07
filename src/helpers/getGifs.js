479adf003643c66f919e7dc80713f6326ef58443
crlsgomezc <crlsgomezcrz332@gmail.com>
2022-03-06 20:55:50 -0500

    Actualizacion en Componente getGif

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=BUR7dSIhDSZWdgrBTT4pcmy2Bwph0BzT`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=20&api_key=BUR7dSIhDSZWdgrBTT4pcmy2Bwph0BzT`;
     const resp = await fetch(url);
     const { data } = await resp.json();
     const gifs = data.map(img => {
